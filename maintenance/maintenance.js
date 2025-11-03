706eaedcb340867695fde703434fb38560ee124d
DPANG <win_dy1207@naver.com>
2025-11-03 13:44:18 +0900

    commit 2025-11-03 13:44:18.65

--- maintenance/maintenance.js
+++ maintenance/maintenance.js
@@ -16,7 +16,7 @@ let combinedQuestions =
   ...maintenance5Questions,
   ...maintenance6Questions, 
   ...maintenance7Questions];
-
+/*
 // question 기준으로 중복 제거
 let uniqueQuestionsMap = new Map();
 
@@ -28,6 +28,7 @@ combinedQuestions.forEach(q => {
 
 // 중복 제거 후 배열로 변환
 let allQuestions = Array.from(uniqueQuestionsMap.values());
+*/
 /*// -----------------------------
 // 문제 배열
 let allQuestions = [
