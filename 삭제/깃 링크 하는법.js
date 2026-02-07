71d91cae039790afcd8046f9b480b563c7688702
KHJ-0201 <ddsp0201@naver.com>
2026-02-07 21:39:05 +0900

    commit 2026-02-07 21:39:05.67

---
+++ 삭제/깃 링크 하는법.js
@@ -0,0 +1,25 @@
+깃 설치(인테넷)
+터미널(CS코드 프로그램) -> git bash
+
+git -v
+
+./auto,bat
+
+git init
+
+git add .
+
+git remote add origin https://github.com/KHJ-0201/J.CAR.CCBBTT
+
+git commit -m "init "
+
+(-> 여기서 명령어가 Run git config --global user.email "you@example.com"이 뜰 경우, 
+git config --global user.email "you@example.com" 를 복사해서 터미널에 붙여넣기. -> you@example.com에 win_dy1207@naver.com 을 입력)
+
+git config --global user.email "ddsp0201@naver.com"
+
+git push origin master
+
+로그인
+
+-> 뭐 여러개 나오고 마지막에 To https://github.com/D2PANG/CBT~~~~ 이렇게 나오면 끝
