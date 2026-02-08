81ef880e73c1254775576b9fc8ba71ba6cc06eab
KHJ-0201 <ddsp0201@naver.com>
2026-02-08 12:47:04 +0900

    Auto Commit: 2026-02-08 12:47:04.12

--- auto.bat
+++ auto.bat
@@ -1,21 +1,20 @@
 @echo off
-REM 파일이 있는 위치로 이동 (작업장 진입)
+:: 한글 깨짐 방지 설정
+chcp 65001 >nul
+
+REM 파일이 있는 위치로 이동
 cd /d "%~dp0"
 
 echo [1/5] 사용자 계정 설정 중...
-:: 어느 컴퓨터에서든 선생님 이름으로 기록되게 합니다.
 git config user.name "KHJ-0201"
 git config user.email "ddsp0201@naver.com"
-:: 로그인 정보를 이 컴퓨터에 안전하게 저장 (하이패스 설정)
 git config credential.helper store
 
 echo [2/5] 저장소 연결 확인 및 초기화...
-:: 이미 설정된 경우 에러를 무시하고 진행합니다.
 if not exist .git (
     git init
 )
 
-:: 리모트 연결이 이미 되어있으면 삭제 후 다시 연결 (충돌 방지)
 git remote remove origin 2>nul
 git remote add origin https://github.com/KHJ-0201/J.CAR.CCBBTT
 
